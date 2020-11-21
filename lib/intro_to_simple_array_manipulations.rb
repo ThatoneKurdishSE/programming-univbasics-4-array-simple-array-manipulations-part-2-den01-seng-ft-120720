ea19e195ab15e1c453e0f22317b3e6bf92559280
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-21 14:04:52 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,20 @@
+def using_concat (first_name="raz", last_name="aljaf")
+first_name.concat last_name
+end
+def using_insert(array, element)
+  array=["raz","is","trying","very","hard","at","life","and"]
+  element="rawa"
+array.insert
+end
+def using_uniq(array)
+  array.uniq
+end
+def using_flatten(array)
+  array.flatten
+end
+def using_delete(array, string)
+  array.delete(string)
+end
+def using_delete_at(array, integer)
+  array.delete_at(integer)
+end
