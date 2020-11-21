9022b36dd5a39bbb231bdaa7c84e065bf3496dce
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-21 14:36:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,7 @@ def using_concat (first_name="raz", last_name="aljaf")
 first_name.concat last_name
 end
 def using_insert(array, key)
-(4).insert
+array.insert(4,key)
 end
 def using_uniq(array)
   array.uniq
